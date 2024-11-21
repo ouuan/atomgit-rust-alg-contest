b12a5ebb5277fd67c208c409616cc57b401924c8
Yufan You <ouuansteve@gmail.com>
2024-11-22 02:06:43 +0800

    feat: solve T9

--- topic1/solutiont9/src/spiral_prime.rs
+++ topic1/solutiont9/src/spiral_prime.rs
@@ -1,3 +1,22 @@
+use prime::miller_rabin_64;
+
 pub fn min_edge_prime_num(number: u32) -> String {
-    todo!()
+    let mut prime_count = 0;
+    let mut total = 1;
+    let mut n = 1;
+
+    for step in (2..).step_by(2) {
+        for _ in 0..4 {
+            n += step;
+            if miller_rabin_64(n) {
+                prime_count += 1;
+            }
+        }
+        total += 4;
+        if prime_count * 100 < total * number {
+            return format!("{},{}", step + 1, prime_count);
+        }
+    }
+
+    unreachable!()
 }
