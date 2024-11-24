e28bc7462f050055e431ff49b886c2172c4e9473
Yufan You <ouuansteve@gmail.com>
2024-11-24 21:43:59 +0800

    feat: solve T7

--- topic1/solutiont7/src/identity_card.rs
+++ topic1/solutiont7/src/identity_card.rs
@@ -1,3 +1,17 @@
+use resident_id::ResidentId;
+
 pub fn check_id_card(id_card_no: &str) -> String {
-    todo!()
+    match id_card_no.parse::<ResidentId>() {
+        Ok(id) => format!(
+            "身份证号码正确,{},{:04}年{:02}月{:02}日,{}-{}-{}",
+            id.gender(),
+            id.birthday().year(),
+            id.birthday().month(),
+            id.birthday().day(),
+            id.province(),
+            id.city(),
+            id.area(),
+        ),
+        Err(_) => "身份证号码错误".into(),
+    }
 }
