c3b7529e3128a8460330654150b967b580493037
Yufan You <ouuansteve@gmail.com>
2024-11-25 16:26:43 +0800

    perf(spiral_prime): skip primality test for square numbers

--- topic1/solutiont9/src/spiral_prime.rs
+++ topic1/solutiont9/src/spiral_prime.rs
@@ -6,12 +6,13 @@ pub fn min_edge_prime_num(number: u32) -> String {
     let mut n = 1;
 
     for step in (2..).step_by(2) {
-        for _ in 0..4 {
+        for _ in 0..3 {
             n += step;
             if miller_rabin_64(n) {
                 prime_count += 1;
             }
         }
+        n += step; // now n is a square number, skipping primality test
         total += 4;
         if prime_count * 100 < total * number {
             return format!("{},{}", step + 1, prime_count);
