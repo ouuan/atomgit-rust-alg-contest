297987e7f9a586f3ca85cce9703f92f67be14d3b
Yufan You <ouuansteve@gmail.com>
2024-11-26 16:33:57 +0800

    feat: solve T10

--- topic1/solutiont10/src/zuc_encryption.rs
+++ topic1/solutiont10/src/zuc_encryption.rs
@@ -1,3 +1,18 @@
+use base64::prelude::*;
+use zuc::cipher::{BlockSizeUser, Unsigned};
+use zuc::prelude::*;
+use zuc::ZucCore;
+
 pub fn encryption(input: String) -> String {
-    todo!()
+    let mut buf = input.into_bytes();
+    const B: usize = <ZucCore as BlockSizeUser>::BlockSize::USIZE;
+    pkcs7_padding::<B>(&mut buf);
+    let mut zuc = Zuc::new(&[0; 16].into(), &[1; 16].into());
+    zuc.apply_keystream(&mut buf);
+    BASE64_STANDARD.encode(buf)
+}
+
+fn pkcs7_padding<const B: usize>(buf: &mut Vec<u8>) {
+    let padding_len = B - buf.len() % B;
+    buf.resize(buf.len() + padding_len, padding_len as u8);
 }
