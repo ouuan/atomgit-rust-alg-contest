137e21605e97eeb1ba7d5dd2949f973520996d4e
Yufan You <ouuansteve@gmail.com>
2024-11-29 02:20:39 +0800

    feat: add crate pinyin

---
+++ crates/pinyin/src/build-data.rs
@@ -0,0 +1,93 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+const CHARACTER_DICT: &str = include_str!("../pinyin-data/pinyin.txt");
+const PHRASE_DICT: &str = include_str!("../phrase-pinyin-data/pinyin.txt");
+
+const TONES: &[(&str, &str)] = &[
+    ("ā", "a"),
+    ("á", "a"),
+    ("ǎ", "a"),
+    ("à", "a"),
+    ("ē", "e"),
+    ("é", "e"),
+    ("ě", "e"),
+    ("è", "e"),
+    ("ō", "o"),
+    ("ó", "o"),
+    ("ǒ", "o"),
+    ("ò", "o"),
+    ("ī", "i"),
+    ("í", "i"),
+    ("ǐ", "i"),
+    ("ì", "i"),
+    ("ū", "u"),
+    ("ú", "u"),
+    ("ǔ", "u"),
+    ("ù", "u"),
+    ("ü", "v"),
+    ("ǖ", "v"),
+    ("ǘ", "v"),
+    ("ǚ", "v"),
+    ("ǜ", "v"),
+    ("ń", "n"),
+    ("ň", "n"),
+    ("ǹ", "n"),
+    ("m̄", "m"),
+    ("ḿ", "m"),
+    ("m̀", "m"),
+    ("ê̄", "ê"),
+    ("ế", "ê"),
+    ("ê̌", "ê"),
+    ("ề", "ê"),
+];
+
+fn remove_tone(pinyin: &str) -> String {
+    let mut pinyin = pinyin.trim().to_string();
+    for (from, to) in TONES {
+        pinyin = pinyin.replace(from, to);
+    }
+    pinyin
+}
+
+fn parse_character_rule(line: &str) -> Option<(String, String)> {
+    let mut parts = line.split('#');
+    let not_comment = parts.next()?;
+    let mut parts = not_comment.split_whitespace();
+    let code = parts.next()?;
+    let code = code.split_at(2).1;
+    let code = code.split_at(code.len() - 1).0;
+    let code = u32::from_str_radix(code, 16).ok()?;
+    let char = std::char::from_u32(code)?;
+    let pinyin = parts.next()?;
+    let pinyin = pinyin.split(',').next()?;
+    let pinyin = remove_tone(pinyin);
+    Some((char.into(), pinyin))
+}
+
+fn parse_phrase_rule(line: &str) -> Option<(String, String)> {
+    let mut parts = line.split('#');
+    let not_comment = parts.next()?;
+    let mut parts = not_comment.split(':');
+    let phrase = parts.next()?;
+    let pinyin = parts.next()?;
+    let pinyin = remove_tone(pinyin);
+    Some((phrase.into(), pinyin))
+}
+
+fn main() {
+    let rules = CHARACTER_DICT
+        .lines()
+        .filter_map(parse_character_rule)
+        .chain(PHRASE_DICT.lines().filter_map(parse_phrase_rule));
+
+    let file = File::create("data/dict.txt").unwrap();
+    let mut writer = BufWriter::new(file);
+    for (phrase, pinyin) in rules {
+        writer.write_all(phrase.as_bytes()).unwrap();
+        writer.write_all(b":").unwrap();
+        writer.write_all(pinyin.as_bytes()).unwrap();
+        writer.write_all(b"\n").unwrap();
+    }
+    writer.flush().unwrap();
+}
---
+++ crates/pinyin/src/lib.rs
@@ -0,0 +1,69 @@
+use max_match_segmentation::{Segment, StringBimm as Converter, StringSegmentation};
+use std::collections::HashMap;
+use std::sync::LazyLock;
+
+mod constants;
+use constants::*;
+
+static WADE_MAP: LazyLock<HashMap<&str, &str>> = LazyLock::new(|| {
+    let mut map = HashMap::new();
+    for &(from, to) in WADE_LIST {
+        if from != to {
+            map.insert(from, to);
+        }
+    }
+    map
+});
+
+static PINYIN_CONVERTER: LazyLock<Converter<&str>> = LazyLock::new(|| {
+    let mut converter = Converter::new();
+    for line in DICT_STR.lines() {
+        let mut parts = line.split(':');
+        let phrase = parts.next().unwrap();
+        let pinyin = parts.next().unwrap();
+        converter.add_phrase(phrase, pinyin);
+    }
+    converter
+});
+
+pub fn han_to_pinyin(han: &str) -> String {
+    let mut result = String::new();
+    for seg in PINYIN_CONVERTER.segmentation(han) {
+        if let Segment::Match(pinyin, _) = seg {
+            if !result.is_empty() {
+                result += " ";
+            }
+            result += pinyin;
+        }
+    }
+    result
+}
+
+pub fn pinyin_to_wade_giles(pinyin: &str) -> String {
+    pinyin
+        .split_whitespace()
+        .map(|p| *WADE_MAP.get(p).unwrap_or(&p))
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+pub fn han_to_wade_giles(han: &str) -> String {
+    pinyin_to_wade_giles(&han_to_pinyin(han))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_han_to_pinyin() {
+        assert_eq!(han_to_pinyin("测试 test"), "ce shi");
+        assert_eq!(han_to_pinyin("欢乐的乐曲"), "huan le de yue qu");
+    }
+
+    #[test]
+    fn test_han_to_wade_giles() {
+        assert_eq!(han_to_wade_giles("清华大学"), "ch'ing hua ta hsüeh");
+        assert_eq!(han_to_wade_giles("北京大学"), "pei ching ta hsüeh");
+    }
+}
