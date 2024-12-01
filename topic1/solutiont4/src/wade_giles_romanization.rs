47ca3caf9d7ed2532d4569992e86833e8151968f
Yufan You <ouuansteve@gmail.com>
2024-12-01 23:15:59 +0800

    refactor(wade_giles_romanization): use slide index instead of split_at

--- topic1/solutiont4/src/wade_giles_romanization.rs
+++ topic1/solutiont4/src/wade_giles_romanization.rs
@@ -2,9 +2,10 @@ use pinyin::han_to_wade_giles;
 
 pub fn converter(input: &str) -> String {
     let mut result = han_to_wade_giles(input);
-    match result.char_indices().nth(1) {
-        None => result.make_ascii_uppercase(),
-        Some((index, _)) => result.split_at_mut(index).0.make_ascii_uppercase(),
+    if let Some((index, _)) = result.char_indices().nth(1) {
+        result[..index].make_ascii_uppercase();
+    } else {
+        result.make_ascii_uppercase();
     }
     result
 }
