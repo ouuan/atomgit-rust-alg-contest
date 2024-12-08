92901962be7ea7c87f74527c2d8a73ca0da90c9f
Yufan You <ouuansteve@gmail.com>
2024-12-08 23:28:13 +0800

    feat: add T11

--- topic1/solutiont11/src/main.rs
+++ topic1/solutiont11/src/main.rs
@@ -1,3 +1,18 @@
+use baby_mimalloc::{new_mimalloc_mmap_mutex, MimallocMmapMutex};
+use std::collections::BTreeMap;
+
+#[global_allocator]
+static ALLOCATOR: MimallocMmapMutex = new_mimalloc_mmap_mutex();
+
 fn main() {
-    println!("Hello, world!");
+    const N: usize = 1_000_000;
+    let mut map = BTreeMap::new();
+    for i in 0..N {
+        map.insert(i.to_string(), i);
+    }
+    assert_eq!(map.get("42"), Some(&42));
+    let vec = map.into_iter().collect::<Vec<_>>();
+    for i in (0..vec.len()).step_by(N / 10) {
+        dbg!(&vec[i]);
+    }
 }
