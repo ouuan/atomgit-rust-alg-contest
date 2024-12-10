63f27fa2f5c9f8555d67be5afc78f9fafdd17677
Yufan You <ouuansteve@gmail.com>
2024-12-10 22:35:59 +0800

    chore(zuc): add mod_lfsr in benchmark

--- crates/zuc/benches/lfsr.rs
+++ crates/zuc/benches/lfsr.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::prelude::*;
 use std::array::from_fn;
 
-fn our_lfsr(a: [u32; 5]) -> u32 {
+pub fn our_lfsr(a: [u32; 5]) -> u32 {
     let mut sum = [
         (a[0] as u64) << 15,
         (a[1] as u64) << 17,
@@ -22,7 +22,16 @@ fn rotate31(x: u32, k: u32) -> u32 {
     ((x << k) | (x >> (31 - k))) & !(1 << 31)
 }
 
-fn naive_lfsr(a: [u32; 5]) -> u32 {
+fn mul31(x: u32, k: u32) -> u32 {
+    (u64::from(x) * (1 << k) % ((1 << 31) - 1)) as u32
+}
+
+fn add31(a: u32, b: u32) -> u32 {
+    let sum = a.wrapping_add(b);
+    (sum & !(1 << 31)).wrapping_add(sum >> 31)
+}
+
+pub fn naive_lfsr(a: [u32; 5]) -> u32 {
     [
         rotate31(a[0], 15),
         rotate31(a[1], 17),
@@ -32,18 +41,30 @@ fn naive_lfsr(a: [u32; 5]) -> u32 {
         a[4],
     ]
     .into_iter()
-    .fold(0, |acc, x| {
-        let sum = acc.wrapping_add(x);
-        (sum & !(1 << 31)).wrapping_add(sum >> 31)
-    })
+    .fold(0, add31)
+}
+
+pub fn mod_lfsr(a: [u32; 5]) -> u32 {
+    [
+        mul31(a[0], 15),
+        mul31(a[1], 17),
+        mul31(a[2], 21),
+        mul31(a[3], 20),
+        mul31(a[4], 8),
+        a[4],
+    ]
+    .into_iter()
+    .fold(0, add31)
 }
 
 fn lfsr_benchmark(c: &mut Criterion) {
     let mut rng = thread_rng();
     let data = from_fn(|_| rng.gen_range(0..(1 << 31)));
     assert_eq!(our_lfsr(data), naive_lfsr(data));
+    assert_eq!(our_lfsr(data), mod_lfsr(data));
     c.bench_function("our", |b| b.iter(|| our_lfsr(black_box(data))));
     c.bench_function("naive", |b| b.iter(|| naive_lfsr(black_box(data))));
+    c.bench_function("mod", |b| b.iter(|| mod_lfsr(black_box(data))));
 }
 
 criterion_group!(benches, lfsr_benchmark);
